afa9d94ec2aefa41bbf2e5bfcf5cc8d9935d0745
Kelly Williams <kelly.mary.williams@gmail.com>
2026-03-26 15:20:16 -0700

    move GenBishop.cpp to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -349,6 +349,56 @@ static void genKing(const Position *p, int from, bool white, Move *moves, int *n
     }
 }
 
+static void genBishop(const Position *p,
+                      int from,
+                      bool white,
+                      const int dirs[][2],  // bishop directions
+                      int dcount,
+                      Move *moves,
+                      int *n)
+{// Ella
+    int r = from / 8;
+    int f = from % 8;
+
+    for (int di = 0; di < dcount; di++)
+    {
+        int df = dirs[di][0];
+        int dr = dirs[di][1];
+
+        int cr = r + dr;
+        int cf = f + df;
+
+        // Slide diagonally until blocked
+        while (cr >= 0 && cr < 8 && cf >= 0 && cf < 8)
+        {
+            int to = cr * 8 + cf;
+            char target = p->b[to];
+
+            if (target == '.')
+            {
+                // Empty square
+                moves[*n] = Move(from, to);
+                (*n)++;
+            }
+            else
+            {
+                // Occupied square
+                bool targetWhite = isWhitePiece(target);
+                if (targetWhite != white)
+                {
+                    // Capture
+                    moves[*n] = Move(from, to);
+                    (*n)++;
+                }
+                // Stop sliding after any piece
+                break;
+            }
+            cr += dr;
+            cf += df;
+        }
+    }
+}
+
 /*============================
 NEW PIECE FUNCTIONS GO HERE
 They MUST be before pseudoLegalMoves
